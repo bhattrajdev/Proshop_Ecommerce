51d046ec8f1ac6494fe191d47e8677589b563dae
bhattrajdev <devrajbhatt010@gmail.com>
2023-08-14 13:23:47 +0545

    controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+import User from "../models/userModel.js";
+import asyncHandler from "express-async-handler";
+
+
+
+const authUser = asyncHandler(async (req, res) => {
+    
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,13 @@
-import express from 'express'
-import Product from '../models/productModel.js';
-import asyncHandler from 'express-async-handler'
+import express from "express";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-// @des Fetch all products
-// @route GET /api/products
-// @access Public
-router.get("/", asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-  res.json(products);
-}));
+router.route("/").get(getProducts);
 
+router.route("/:id").get(getProductById);
 
-// @des Fetch single product
-// @route GET /api/products/"id"
-// @access Public
-router.get("/:id",asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
-  if(product){
-    res.json(product)
-  }else{
-    res.status(404)
-    throw new Error('Product not found')
-  }
-  res.json(product);
-}));
-
-
-
-export default router
-
+export default router;
