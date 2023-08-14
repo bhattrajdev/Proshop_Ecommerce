eeb77814b7bd44da5d849d9211a96b5cf88bcb6d
bhattrajdev <devrajbhatt010@gmail.com>
2023-08-15 00:26:53 +0545

    user part done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,6 +19,42 @@ const authUser = asyncHandler(async (req, res) => {
         throw new Error('Invalid email or password')
     }
 });
+// for new user register
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name,email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if(userExists){
+    res.status(400)
+    throw new console.Error('User already exists');
+  }
+
+  const users = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(userExists){
+    res.status(201).res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  }else{
+
+    res.status(400)
+    throw new Error('Invalid User data')
+
+  }
+});
+
+
+
+
 
 // for user profile
 
@@ -38,4 +74,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export {authUser, getUserProfile}
+export {authUser, getUserProfile, registerUser}
