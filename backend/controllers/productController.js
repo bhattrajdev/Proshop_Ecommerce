7e37931e39099bdfb581488c6cec0159a9586eb7
bhattrajdev <devrajbhatt010@gmail.com>
2023-09-03 23:57:48 +0545

    create and update product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,42 +1,78 @@
 import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
 
-
-
 // @des Fetch all products
 // @route GET /api/products
 // @access Public
 
-const getProducts = asyncHandler(async(req,res) =>{
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
-})
+});
 
-
-const getProductById = asyncHandler(async(req,res) =>{
- const product = await Product.findById(req.params.id);
- if (product) {
-   res.json(product);
- } else {
-   res.status(404);
-   throw new Error("Product not found");
- }
- res.json(product);
-})
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  res.json(product);
+});
 
 // to delete product
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
-      await product.deleteOne();
-      res.json({ message: "Product Removed" });
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-
+    await product.deleteOne();
+    res.json({ message: "Product Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product Not Found");
+  }
 });
 
+// to create product
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
 
+  const createdProduct = await product.save();
+  res.status(201).json(createProduct);
+});
 
-export {getProducts,getProductById,deleteProduct}
+// to create product
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(201).json(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product Not Found");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct,createProduct,updateProduct };
