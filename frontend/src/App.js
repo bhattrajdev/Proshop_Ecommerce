6b3badf57f001ab3066567d3664ca691abb2ccf3
bhattrajdev <devrajbhatt010@gmail.com>
2023-09-03 22:27:57 +0545

    admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+
 
 const App = () => {
   return (
@@ -34,6 +36,7 @@ const App = () => {
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id?" element={<CartScreen />} />
             <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
           </Routes>
         </Container>
